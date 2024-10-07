d95b16714231414e325e16b394ed591e3fcb17f1
Yang Pan <yangp2@andrew.cmu.edu>
2024-10-07 10:29:11 -0400

    implement support for query 'what is your andrew id?'

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,5 +7,9 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("andrew id")) {
+    return ("My Andrew ID is yangp2"); 
+  }
+
   return "";
 }
